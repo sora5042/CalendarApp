820e436b6792fcf714ad9685789f36d48cd0cb20
ooya sora <esuora5042@gmail.com>
2022-01-06 13:02:17 +0900

    Widgetを実装してみたいと思い、追加した

---
+++ Widget/TestWidget.swift
@@ -0,0 +1,20 @@
+//
+//  TestWidget.swift
+//  CalendarApp
+//
+//  Created by 大谷空 on 2022/01/06.
+//
+
+import SwiftUI
+
+struct TestWidget: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        TestWidget()
+    }
+}
