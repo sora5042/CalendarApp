e0e23777b3eab26debe645e4be3be3441b61a5a4
ooya sora <esuora5042@gmail.com>
2022-02-23 16:19:40 +0900

    横画面非対応にしていたが、これがないと横画面になってしまいレイアウトを崩れてしまうので追加した

--- CalendarApp/ViewControllers/Setting/SettingViewController.swift
+++ CalendarApp/ViewControllers/Setting/SettingViewController.swift
@@ -28,4 +28,8 @@ class SettingViewController: UIViewController {
     override var shouldAutorotate: Bool {
         return false
     }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
 }
